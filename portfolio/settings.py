7ad636c84685e082bd112dd154c6c3b65f519634
Muhamad Fahrur Rasyid <mfahrurashyid@gmail.com>
2020-06-21 23:11:29 -0400

    Adding Docker Container and setup Postgresql

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u-!+38$2d_y#@vc-l*&0t-$gz#_5pgg#s5jkm$7&a^eu)hvxi#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio',
+        'USER': 'devuser',
+        'PASSWORD': 'devpass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
